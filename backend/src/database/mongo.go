c975b61eb77613b5b5630e4095644fd34a1bb6b0
Joshua <tan.yin.feng@hotmail.com>
2024-01-20 15:59:16 +0800

    refactor: simplify to only return mongo client

--- backend/src/database/mongo.go
+++ backend/src/database/mongo.go
@@ -2,35 +2,31 @@ package database
 
 import (
 	"context"
-	"log"
 	"os"
 
+	"github.com/joshtyf/flowforge/src/logger"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type MongoClientWrapper struct {
-	client *mongo.Client
-}
+const (
+	DatabaseName = "flowforge_db"
+)
 
 var c *mongo.Client
 
-func init() {
-	uri := os.Getenv("MONGO_URI")
-	if uri == "" {
-		log.Fatal("MONGO_URI environment variable not set") // TODO: replace with custom logger
+func GetMongoClient() (*mongo.Client, error) {
+	if c == nil {
+		uri := os.Getenv("MONGO_URI")
+		if uri == "" {
+			logger.Error("MONGO_URI environment variable not set", nil)
+		}
+		client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+		if err != nil {
+			return nil, err
+		}
+		c = client
 	}
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
-	if err != nil {
-		log.Fatalf("failed to create mongo client, err=%v", err) // TODO: replace with custom logger
-	}
-	c = client
-}
 
-func NewMongoClientWrapper() *MongoClientWrapper {
-	return &MongoClientWrapper{client: c}
-}
-
-func (mcw *MongoClientWrapper) GetClient() *mongo.Client {
-	return mcw.client
+	return c, nil
 }
