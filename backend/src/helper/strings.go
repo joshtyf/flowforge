855a82d560d67aa57ab27d5a85b49297b962f832
Joshua <tan.yin.feng@hotmail.com>
2024-01-20 20:42:42 +0800

    feat: create string helper functions

---
+++ backend/src/helper/strings.go
@@ -0,0 +1,33 @@
+package helper
+
+import (
+	"regexp"
+)
+
+func ExtractParameterFromString(s string) []string {
+	re := regexp.MustCompile(`\${(.+?)}`)
+	matches := re.FindAllStringSubmatch(s, -1)
+
+	var words []string
+	for _, match := range matches {
+		if len(match) == 2 {
+			words = append(words, match[1])
+		}
+	}
+
+	return words
+}
+
+func StringSliceEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
