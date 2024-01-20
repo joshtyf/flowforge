7a7cdbc9b154805ece49b84e467a2ada370a36ea
Zheng_Zhi_Qiang <zqzheng98@gmail.com>
2024-01-20 14:15:03 +0800

    refactor: added env var for mongdb name

--- backend/src/main.go
+++ backend/src/main.go
@@ -10,5 +10,8 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/api/healthcheck", handlers.HealthCheck).Methods("GET")
+	r.HandleFunc("/api/servicerequest/new", handlers.CreateServiceRequest).Methods("POST").Headers("Content-Type", "application/json")
+	r.HandleFunc("/api/servicerequest/{requestId}", handlers.GetServiceRequest).Methods("GET")
+	r.HandleFunc("/api/servicerequest", handlers.GetAllServiceRequest).Methods("GET")
 	http.ListenAndServe(":8080", r)
 }
