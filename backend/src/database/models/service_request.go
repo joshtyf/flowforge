9361ca5b141ebde432d6a5612b4ea22996c093d9
Joshua <tan.yin.feng@hotmail.com>
2024-01-24 22:12:42 +0800

    refactor: create custom type for ServiceRequestStatus

--- backend/src/database/models/service_request.go
+++ backend/src/database/models/service_request.go
@@ -6,13 +6,22 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type ServiceRequestStatus string
+
+const (
+	Pending ServiceRequestStatus = "Pending"
+	Running ServiceRequestStatus = "Running"
+	Success ServiceRequestStatus = "Success"
+	Failure ServiceRequestStatus = "Failure"
+)
+
 type ServiceRequestModel struct {
-	Id              primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	PipelineId      string             `bson:"pipeline_id" json:"pipeline_id"`
-	PipelineVersion int                `bson:"pipeline_version" json:"pipeline_version"`
-	Status          Status             `bson:"status" json:"status"`
-	CreatedOn       time.Time          `bson:"created_on" json:"created_on"`
-	LastUpdated     time.Time          `bson:"last_updated" json:"last_updated"`
-	Remarks         string             `bson:"remarks" json:"remarks"`
+	Id              primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
+	PipelineId      string               `bson:"pipeline_uuid" json:"pipeline_uuid"`
+	PipelineVersion int                  `bson:"pipeline_version" json:"pipeline_version"`
+	Status          ServiceRequestStatus `bson:"status" json:"status"`
+	CreatedOn       time.Time            `bson:"created_on" json:"created_on"`
+	LastUpdated     time.Time            `bson:"last_updated" json:"last_updated"`
+	Remarks         string               `bson:"remarks" json:"remarks"`
 	// FormData        FormData           `bson:"form_data"`
 }
