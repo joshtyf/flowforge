e05cfcfc5900a0a5cdcddb7535db995675c66580
Lim Zi Yang <yang7332@gmail.com>
2024-01-24 21:01:21 +0800

    chore: add todo comments

--- frontend/src/app/(authenticated)/service-catalog/create-service/_hooks/useCreateService.ts
+++ frontend/src/app/(authenticated)/service-catalog/create-service/_hooks/useCreateService.ts
@@ -22,8 +22,12 @@ const useCreateService = () => {
     useState<object>(BASIC_SERVICE_OBJECT)
 
   const handleSubmitObject = () => {
+    // TODO: Replace with API call
+    // TODO: Add loading for submit and re-direct to dashboard upon submission
     console.log("Submitting:", serviceObject)
   }
+
+  // TODO: Add validation of JSON object on edit/add
 
   return { serviceObject, setServiceObject, handleSubmitObject }
 }
