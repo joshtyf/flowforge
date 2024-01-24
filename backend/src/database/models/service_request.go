369775bdb29ed38b6732b5873cfe04643f21b3fa
Joshua <tan.yin.feng@hotmail.com>
2024-01-24 21:15:52 +0800

    refactor: add json embedded type

--- backend/src/database/models/service_request.go
+++ backend/src/database/models/service_request.go
@@ -7,12 +7,12 @@ import (
 )
 
 type ServiceRequestModel struct {
-	Id              primitive.ObjectID `bson:"_id,omitempty"`
-	PipelineId      string             `bson:"pipeline_id"`
-	PipelineVersion int                `bson:"pipeline_version"`
-	Status          string             `bson:"status"`
-	CreatedOn       time.Time          `bson:"created_on"`
-	LastUpdated     time.Time          `bson:"last_updated"`
-	Remarks         string             `bson:"remarks"`
+	Id              primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	PipelineId      string             `bson:"pipeline_id" json:"pipeline_id"`
+	PipelineVersion int                `bson:"pipeline_version" json:"pipeline_version"`
+	Status          Status             `bson:"status" json:"status"`
+	CreatedOn       time.Time          `bson:"created_on" json:"created_on"`
+	LastUpdated     time.Time          `bson:"last_updated" json:"last_updated"`
+	Remarks         string             `bson:"remarks" json:"remarks"`
 	// FormData        FormData           `bson:"form_data"`
 }
