918c68180d6b14645b679e9298193b722dcc63a2
Joshua <tan.yin.feng@hotmail.com>
2024-02-03 16:36:53 +0800

    refactor: update handlers to reduce code repetition

---
+++ backend/src/api/errors.go
@@ -0,0 +1,15 @@
+package api
+
+import "errors"
+
+var (
+	ErrInternalServerError = errors.New("internal server error")
+	ErrWrongStepType       = errors.New("wrong step type")
+	ErrJsonParseError      = errors.New("unable to parse json request body")
+
+	ErrPipelineCreateFail = errors.New("failed to create pipeline")
+
+	ErrServiceRequestNotStarted       = errors.New("service request not started")
+	ErrServiceRequestAlreadyStarted   = errors.New("service request already started")
+	ErrServiceRequestAlreadyCompleted = errors.New("service request already completed")
+)
--- backend/src/main.go
+++ backend/src/main.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	handlers "github.com/joshtyf/flowforge/src/api/handlers"
+	"github.com/joshtyf/flowforge/src/api"
 	"github.com/joshtyf/flowforge/src/execute"
 )
 
@@ -16,16 +16,16 @@ func main() {
 	srm.Start()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/api/healthcheck", handlers.HealthCheck).Methods("GET")
-	r.HandleFunc("/api/service_request", handlers.CreateServiceRequest).Methods("POST").Headers("Content-Type", "application/json")
-	r.HandleFunc("/api/service_request/{requestId}", handlers.GetServiceRequest).Methods("GET")
-	r.HandleFunc("/api/service_request", handlers.GetAllServiceRequest).Methods("GET")
-	r.HandleFunc("/api/service_request/{requestId}/start", handlers.StartServiceRequest).Methods("GET")
-	r.HandleFunc("/api/service_request/{requestId}/approve", handlers.ApproveServiceRequest).Methods("POST").Headers("Content-Type", "application/json")
-	r.HandleFunc("/api/service_request/{requestId}/cancel", handlers.CancelStartedServiceRequest).Methods("GET")
-	r.HandleFunc("/api/service_request/{requestId}", handlers.UpdateServiceRequest).Methods("PATCH").Headers("Content-Type", "application/json")
-	r.HandleFunc("/api/pipeline", handlers.CreatePipeline).Methods("POST").Headers("Content-Type", "application/json")
-	r.HandleFunc("/api/pipeline", handlers.GetAllPipelines).Methods("GET")
-	r.HandleFunc("/api/pipeline/{pipelineId}", handlers.GetPipeline).Methods("GET")
+	r.HandleFunc("/api/healthcheck", api.NewHandler(api.HealthCheck)).Methods("GET")
+	r.HandleFunc("/api/service_request", api.NewHandler(api.CreateServiceRequest)).Methods("POST").Headers("Content-Type", "application/json")
+	r.HandleFunc("/api/service_request/{requestId}", api.NewHandler(api.GetServiceRequest)).Methods("GET")
+	r.HandleFunc("/api/service_request", api.NewHandler(api.GetAllServiceRequest)).Methods("GET")
+	r.HandleFunc("/api/service_request/{requestId}/approve", api.NewHandler(api.ApproveServiceRequest)).Methods("POST").Headers("Content-Type", "application/json")
+	r.HandleFunc("/api/service_request/{requestId}/cancel", api.NewHandler(api.CancelStartedServiceRequest)).Methods("GET")
+	r.HandleFunc("/api/service_request/{requestId}/start", api.NewHandler(api.StartServiceRequest)).Methods("GET")
+	r.HandleFunc("/api/service_request/{requestId}", api.NewHandler(api.UpdateServiceRequest)).Methods("PATCH").Headers("Content-Type", "application/json")
+	r.HandleFunc("/api/pipeline", api.NewHandler(api.CreatePipeline)).Methods("POST").Headers("Content-Type", "application/json")
+	r.HandleFunc("/api/pipeline", api.NewHandler(api.GetAllPipelines)).Methods("GET")
+	r.HandleFunc("/api/pipeline/{pipelineId}", api.NewHandler(api.GetPipeline)).Methods("GET")
 	http.ListenAndServe(":8080", r)
 }
