8e0bf9928292919c8ea7f0a1ee75d05406609bbb
Joshua <tan.yin.feng@hotmail.com>
2024-02-25 16:40:59 +0800

    refactor: rename 'api' package to 'server' package

--- backend/src/main.go
+++ backend/src/main.go
@@ -5,8 +5,8 @@ import (
 
 	gorillaHandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/joshtyf/flowforge/src/api"
 	"github.com/joshtyf/flowforge/src/execute"
+	"github.com/joshtyf/flowforge/src/server"
 )
 
 func main() {
@@ -17,20 +17,20 @@ func main() {
 	srm.Start()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/api/healthcheck", api.NewHandler(api.HealthCheck)).Methods("GET")
-	r.HandleFunc("/api/service_request", api.NewHandler(api.CreateServiceRequest)).Methods("POST").Headers("Content-Type", "application/json")
-	r.HandleFunc("/api/service_request/{requestId}", api.NewHandler(api.GetServiceRequest)).Methods("GET")
-	r.HandleFunc("/api/service_request", api.NewHandler(api.GetAllServiceRequest)).Methods("GET")
-	r.HandleFunc("/api/service_request/{requestId}/approve", api.NewHandler(api.ApproveServiceRequest)).Methods("POST").Headers("Content-Type", "application/json")
-	r.HandleFunc("/api/service_request/{requestId}/cancel", api.NewHandler(api.CancelStartedServiceRequest)).Methods("GET")
-	r.HandleFunc("/api/service_request/{requestId}/start", api.NewHandler(api.StartServiceRequest)).Methods("GET")
-	r.HandleFunc("/api/service_request/{requestId}", api.NewHandler(api.UpdateServiceRequest)).Methods("PATCH").Headers("Content-Type", "application/json")
-	r.HandleFunc("/api/pipeline", api.NewHandler(
-		api.CreatePipeline,
-		api.ValidateCreatePipelineRequest,
+	r.HandleFunc("/api/healthcheck", server.NewHandler(server.HealthCheck)).Methods("GET")
+	r.HandleFunc("/api/service_request", server.NewHandler(server.CreateServiceRequest)).Methods("POST").Headers("Content-Type", "application/json")
+	r.HandleFunc("/api/service_request/{requestId}", server.NewHandler(server.GetServiceRequest)).Methods("GET")
+	r.HandleFunc("/api/service_request", server.NewHandler(server.GetAllServiceRequest)).Methods("GET")
+	r.HandleFunc("/api/service_request/{requestId}/approve", server.NewHandler(server.ApproveServiceRequest)).Methods("POST").Headers("Content-Type", "application/json")
+	r.HandleFunc("/api/service_request/{requestId}/cancel", server.NewHandler(server.CancelStartedServiceRequest)).Methods("GET")
+	r.HandleFunc("/api/service_request/{requestId}/start", server.NewHandler(server.StartServiceRequest)).Methods("GET")
+	r.HandleFunc("/api/service_request/{requestId}", server.NewHandler(server.UpdateServiceRequest)).Methods("PATCH").Headers("Content-Type", "application/json")
+	r.HandleFunc("/api/pipeline", server.NewHandler(
+		server.CreatePipeline,
+		server.ValidateCreatePipelineRequest,
 	)).Methods("POST").Headers("Content-Type", "application/json")
-	r.HandleFunc("/api/pipeline", api.NewHandler(api.GetAllPipelines)).Methods("GET")
-	r.HandleFunc("/api/pipeline/{pipelineId}", api.NewHandler(api.GetPipeline)).Methods("GET")
+	r.HandleFunc("/api/pipeline", server.NewHandler(server.GetAllPipelines)).Methods("GET")
+	r.HandleFunc("/api/pipeline/{pipelineId}", server.NewHandler(server.GetPipeline)).Methods("GET")
 	http.ListenAndServe(":8080", gorillaHandlers.CORS(
 		gorillaHandlers.AllowedOrigins([]string{"http://localhost:3000"}),
 		gorillaHandlers.AllowedHeaders([]string{
