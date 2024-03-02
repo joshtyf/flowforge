87f47e27887854d843f048f2a4dd18ff15286a69
Joshua <tan.yin.feng@hotmail.com>
2024-03-02 19:47:56 +0800

    feat: add Create method

---
+++ backend/src/database/service_request_event.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/joshtyf/flowforge/src/database/models"
+)
+
+type ServiceRequestEvent struct {
+	db *sql.DB
+}
+
+func NewServiceRequestEvent(db *sql.DB) *ServiceRequestEvent {
+	return &ServiceRequestEvent{
+		db: db,
+	}
+}
+
+func (sre *ServiceRequestEvent) Create(srem *models.ServiceRequestEventModel) error {
+	queryStr := "INSERT INTO service_request_event (event_type, service_request_id, step_name, approved_by) VALUES ($1, $2, $3, $4)"
+	err := sre.db.QueryRow(
+		queryStr,
+		srem.EventType,
+		srem.ServiceRequestId,
+		srem.StepName,
+		srem.ApprovedBy,
+	).Scan()
+	if err != nil {
+		return err
+	}
+	return nil
+}
