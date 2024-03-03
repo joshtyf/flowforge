cf8db33b2d67b920e251b4c7187de39130ac08f9
Aaron Lee <lyl.aaron.98@gmail.com>
2024-03-03 23:35:46 +0800

    Update layout.tsx
    reconfigured layout.tsx to avoid re-creating it on each render

--- frontend/src/app/(authenticated)/layout.tsx
+++ frontend/src/app/(authenticated)/layout.tsx
@@ -1,43 +1,50 @@
-"use client"
+// components/layouts/authenticated-layout.tsx
 
-import Navbar from "@/components/layouts/navbar"
-import Sidebar from "@/components/layouts/sidebar"
-import { Toaster } from "@/components/ui/toaster"
-import React, { ReactNode, useState } from "react"
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query"
+import Navbar from "@/components/layouts/navbar";
+import Sidebar from "@/components/layouts/sidebar";
+import { Toaster } from "@/components/ui/toaster";
+import React, { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 interface AuthenticatedLayoutProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 export default function AuthenticatedLayout({
   children,
 }: AuthenticatedLayoutProps) {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const toggleSidebar = () => {
-    setIsSidebarOpen((isSidebarOpen) => !isSidebarOpen)
-  }
+    setIsSidebarOpen((isSidebarOpen) => !isSidebarOpen);
+  };
 
   return (
     <QueryClientProvider client={queryClient}>
       <div className="flex flex-row w-full min-h-[100vh]">
+        {/* Sidebar */}
         <Sidebar
-          className={` ${isSidebarOpen ? "min-w-[280px] w-[280px]" : "min-w-0 w-0"} overflow-x-hidden transition-width duration-300 ease-in-out`}
+          className={`${
+            isSidebarOpen ? "min-w-[280px] w-[280px]" : "min-w-0 w-0"
+          } overflow-x-hidden transition-width duration-300 ease-in-out`}
         />
+
+        {/* Main Content */}
         <div className="w-full">
+          {/* Navbar */}
           <Navbar toggleSidebar={toggleSidebar} />
+
+          {/* Main Content Wrapper */}
           <div className="w-full h-full max-h-[90vh] flex justify-center items-center flex-col relative">
             <div className="w-5/6 h-full relative">{children}</div>
           </div>
         </div>
+
+        {/* Toaster */}
         <Toaster />
       </div>
     </QueryClientProvider>
-  )
+  );
 }
