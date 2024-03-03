db20ecb3e96e4f687b2a2eab168b82dfb0573851
Aaron Lee <lyl.aaron.98@gmail.com>
2024-03-04 00:19:30 +0800

    Create modular components for Signup page
    Refactor the Signup page by isolating form-related logic and UI elements into modular components.
    Introduced components in the form, layout, and ui directories for better code organization.

---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import SignupForm from "../components/form/SignupForm";
+import PageLayout from "../components/layout/PageLayout";
+import InputField from "../components/ui/InputField";
+
+export default function SignupPage() {
+  const [loading, setLoading] = useState(false);
+
+  const onSignup = async (user) => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      toast.success("Signup success");
+      // router.push("/login");
+    } catch (error) {
+      console.log("Signup failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <PageLayout title={loading ? "Processing" : "Signup"} loading={loading}>
+      <SignupForm onSignup={onSignup} />
+      <Link href="/login">Visit login page</Link>
+    </PageLayout>
+  );
+}
