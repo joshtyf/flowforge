0121fdbb0f963582e240206490168349b3cf3364
Aaron Lee <lyl.aaron.98@gmail.com>
2024-03-04 00:10:40 +0800

    created components for login
    isolated the components from the full page.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,72 +1,53 @@
-"use client";
 import Link from "next/link";
-import React, {useEffect} from "react";
-import {useRouter} from "next/navigation";
+import React, { useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import LoginForm from "../components/form/loginForm";
+import PageLayout from "../components/layout/pageLayout";
+import InputField from "../components/ui/inputField";
 
 export default function LoginPage() {
-    const router = useRouter();
-    const [user, setUser] = React.useState({
-        email: "",
-        password: "",
-       
-    })
-    const [buttonDisabled, setButtonDisabled] = React.useState(false);
-    const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = useState(false);
 
-
-    const onLogin = async () => {
-        try {
-            setLoading(true);
-            const response = await axios.post("/api/users/login", user);
-            console.log("Login success", response.data);
-            toast.success("Login success");
-            router.push("/profile");
-        } catch (error:any) {
-            console.log("Login failed", error.message);
-            toast.error(error.message);
-        } finally{
-        setLoading(false);
-        }
+  const onLogin = async (user) => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      // router.push("/profile");
+    } catch (error) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        if(user.email.length > 0 && user.password.length > 0) {
-            setButtonDisabled(false);
-        } else{
-            setButtonDisabled(true);
-        }
-    }, [user]);
-
-    return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1>{loading ? "Processing" : "Login"}</h1>
-        <hr />
-        
-        <label htmlFor="email">email</label>
-        <input 
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            id="email"
-            type="text"
-            value={user.email}
-            onChange={(e) => setUser({...user, email: e.target.value})}
-            placeholder="email"
-            />
-        <label htmlFor="password">password</label>
-        <input 
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            id="password"
-            type="password"
-            value={user.password}
-            onChange={(e) => setUser({...user, password: e.target.value})}
-            placeholder="password"
-            />
-            <button
-            onClick={onLogin}
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
-            <Link href="/signup">Visit Signup page</Link>
-        </div>
-    )
-
+  return (
+    <PageLayout title={loading ? "Processing" : "Login"} loading={loading}>
+      <LoginForm onLogin={onLogin} />
+      <InputField
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(value) => setUser({ ...user, email: value })}
+        placeholder="email"
+      />
+      <InputField
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(value) => setUser({ ...user, password: value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Login here
+      </button>
+      <Link href="/signup">Visit Signup page</Link>
+    </PageLayout>
+  );
 }
