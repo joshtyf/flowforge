7a0616c5d8d9e2ebc63363d8450ddc04e68b120a
Lim Zi Yang <yang7332@gmail.com>
2024-03-03 17:05:21 +0800

    feat: add rejected dummy sr to dashboard

--- frontend/src/app/(authenticated)/service-request-dashboard/_hooks/use-service-requests.ts
+++ frontend/src/app/(authenticated)/service-request-dashboard/_hooks/use-service-requests.ts
@@ -64,6 +64,22 @@ const DUMMY_SERVICE_REQUESTS: ServiceRequest[] = [
     },
   },
   {
+    id: "4",
+    pipeline_id: "65d48c02d62a1281c4f4ba3e",
+    pipeline_version: "0",
+    status: ServiceRequestStatus.REJECTED,
+    created_on: "2024-02-21T00:00:00",
+    last_updated: "2024-02-21T00:00:00",
+
+    remarks: "",
+    form_data: {
+      user_id: "1",
+      user_name: "Test User",
+      name: "Pipeline 1",
+      description: "Pipeline 1 description",
+    },
+  },
+  {
     id: "5",
     pipeline_id: "65d48c02d62a1281c4f4ba3e",
     pipeline_version: "0",
