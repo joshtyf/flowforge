813efe0c202c2d7333405111d86840778fb80178
Aaron Lee <lyl.aaron.98@gmail.com>
2024-03-06 17:51:37 +0800

    updated PR issues in layout.tsx
    updated some of the PR issues highlighted

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -45,7 +45,7 @@ export default function LoginPage() {
         onClick={onLogin}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
-        Login here
+        Login
       </button>
       <Link href="/signup">Visit Signup page</Link>
     </PageLayout>
