083fe24f2aa6f6e645c0757202f10f42cb481e30
Lim Zi Yang <yang7332@gmail.com>
2024-03-10 15:21:42 +0800

    chore: remove unused imports

--- frontend/src/app/login/callback/page.tsx
+++ frontend/src/app/login/callback/page.tsx
@@ -1,12 +1,8 @@
 "use client"
 
-import { setCookie } from "cookies-next"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
 import useAuthentication from "./_hooks/useAuthentication"
 
 export default function LoginCallbackPage() {
-  const router = useRouter()
   const TIMEOUT_DURATION = 5
 
   useAuthentication({ timeout: TIMEOUT_DURATION })
