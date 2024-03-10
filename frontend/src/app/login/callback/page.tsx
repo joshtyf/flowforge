ad67e16249d4b57220fac7682e54d7b35650b74e
Lim Zi Yang <yang7332@gmail.com>
2024-03-10 16:15:02 +0800

    refactor: update hook name

--- frontend/src/app/login/callback/page.tsx
+++ frontend/src/app/login/callback/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import useAuthentication from "./_hooks/useAuthentication"
+import useAuth from "./_hooks/useAuth"
 
 export default function LoginCallbackPage() {
   const TIMEOUT_DURATION = 5
 
-  useAuthentication({ timeout: TIMEOUT_DURATION })
+  useAuth({ timeout: TIMEOUT_DURATION })
   return <div>Successfully logged in! Redirecting in {TIMEOUT_DURATION}s</div>
 }
