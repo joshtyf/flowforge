536b80a367d7b87931ab6f41dcb248c2705f93c3
Lim Zi Yang <yang7332@gmail.com>
2024-03-10 14:48:39 +0800

    feat: render authenticated only if logged in

--- frontend/src/app/(authenticated)/layout.tsx
+++ frontend/src/app/(authenticated)/layout.tsx
@@ -33,18 +33,20 @@ export default function AuthenticatedLayout({
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex flex-row w-full min-h-[100vh]">
-        <Sidebar
-          className={` ${isSidebarOpen ? "min-w-[280px] w-[280px]" : "min-w-0 w-0"} overflow-x-hidden transition-width duration-300 ease-in-out`}
-        />
-        <div className="w-full">
-          <Navbar toggleSidebar={toggleSidebar} />
-          <div className="w-full h-full max-h-[90vh] flex justify-center items-center flex-col relative">
-            <div className="w-5/6 h-full relative">{children}</div>
+      {isLoggedIn && (
+        <div className="flex flex-row w-full min-h-[100vh]">
+          <Sidebar
+            className={` ${isSidebarOpen ? "min-w-[280px] w-[280px]" : "min-w-0 w-0"} overflow-x-hidden transition-width duration-300 ease-in-out`}
+          />
+          <div className="w-full">
+            <Navbar toggleSidebar={toggleSidebar} />
+            <div className="w-full h-full max-h-[90vh] flex justify-center items-center flex-col relative">
+              <div className="w-5/6 h-full relative">{children}</div>
+            </div>
           </div>
+          <Toaster />
         </div>
-        <Toaster />
-      </div>
+      )}
     </QueryClientProvider>
   )
 }
