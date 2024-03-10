879ee22e1d736f9390c9c64d2bf60fdccb9de2f5
Lim Zi Yang <yang7332@gmail.com>
2024-03-10 16:20:02 +0800

    refactor: extract base url to env variables

--- frontend/src/lib/auth0.ts
+++ frontend/src/lib/auth0.ts
@@ -3,6 +3,14 @@ export const getAuth0AuthorizeLink = () => {
     `https://${process.env.AUTH0_DOMAIN}/authorize?` +
     `client_id=${process.env.AUTH0_CLIENT_ID}&` +
     `response_type=token&` +
-    `redirect_uri=http://localhost:3000/login/callback` // TODO: update with production URL
+    `redirect_uri=${process.env.APP_BASE_URL}/login/callback` // TODO: update with production URL
+  return url
+}
+
+export const getAuth0LogoutLink = () => {
+  const url =
+    `https://${process.env.AUTH0_DOMAIN}/v2/logout?` +
+    `client_id=${process.env.AUTH0_CLIENT_ID}&` +
+    `returnTo=${process.env.APP_BASE_URL}/login` // TODO: update with production URL
   return url
 }
