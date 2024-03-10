5651d04d01f6bcbf71ca1f7f737ac55266478286
Lim Zi Yang <yang7332@gmail.com>
2024-03-10 15:21:18 +0800

    refactor: extract authentication into hook

---
+++ frontend/src/app/login/callback/_hooks/useAuthentication.ts
@@ -0,0 +1,29 @@
+import { setCookie } from "cookies-next"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+const TIMEOUT_DURATION = 5
+
+interface UseAuthenticationOptions {
+  timeout?: number
+}
+
+const useAuthentication = ({ timeout = 5 }: UseAuthenticationOptions) => {
+  const router = useRouter()
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      console.log("redirecting")
+      router.replace("/")
+    }, TIMEOUT_DURATION * 1000)
+    const hash = window.location.hash.substring(1)
+    const search = new URLSearchParams(hash)
+    const accessToken = search.get("access_token")
+    const expiresIn = search.get("expires_in")
+    const tokenType = search.get("token_type")
+    setCookie("loggedIn", "true")
+
+    return () => clearTimeout(timeout)
+  }, [router])
+  return {}
+}
+
+export default useAuthentication
--- frontend/src/app/login/callback/page.tsx
+++ frontend/src/app/login/callback/page.tsx
@@ -3,24 +3,12 @@
 import { setCookie } from "cookies-next"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
+import useAuthentication from "./_hooks/useAuthentication"
 
 export default function LoginCallbackPage() {
   const router = useRouter()
   const TIMEOUT_DURATION = 5
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      console.log("redirecting")
-      router.replace("/")
-    }, TIMEOUT_DURATION * 1000)
-    const hash = window.location.hash.substring(1)
-    const search = new URLSearchParams(hash)
-    const accessToken = search.get("access_token")
-    const expiresIn = search.get("expires_in")
-    const tokenType = search.get("token_type")
-    setCookie("loggedIn", "true")
-
-    return () => clearTimeout(timeout)
-  }, [router])
+  useAuthentication({ timeout: TIMEOUT_DURATION })
   return <div>Successfully logged in! Redirecting in {TIMEOUT_DURATION}s</div>
 }
