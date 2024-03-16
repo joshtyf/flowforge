9217e3fb1603284451b02ec6ee6751792efd57d8
Joshua <tan.yin.feng@hotmail.com>
2024-03-16 17:01:43 +0800

    feat: create GetStepsLatestEvent method

--- backend/src/database/service_request_event.go
+++ backend/src/database/service_request_event.go
@@ -27,3 +27,38 @@ func (sre *ServiceRequestEvent) Create(srem *models.ServiceRequestEventModel) er
 	)
 	return err
 }
+
+func (sre *ServiceRequestEvent) GetStepsLatestEvent(serviceReequestId string) ([]*models.ServiceRequestEventModel, error) {
+	queryStr := `
+		WITH LatestEvents AS (
+			SELECT *, ROW_NUMBER() OVER (PARTITION BY step_name ORDER BY created_at DESC) AS row_num
+			FROM service_request_event
+			WHERE service_request_id = $1
+		)
+		SELECT event_id, event_type, service_request_id, step_name, approved_by, created_at FROM LatestEvents
+		WHERE row_num = 1;`
+
+	rows, err := sre.db.Query(queryStr, serviceReequestId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var srems []*models.ServiceRequestEventModel
+	for rows.Next() {
+		srem := &models.ServiceRequestEventModel{}
+		err := rows.Scan(
+			&srem.EventId,
+			&srem.EventType,
+			&srem.ServiceRequestId,
+			&srem.StepName,
+			&srem.ApprovedBy,
+			&srem.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		srems = append(srems, srem)
+	}
+	return srems, nil
+}
