2532768bce8e14f9d9ea557278e9233b3a67e978
Lim Zi Yang <yang7332@gmail.com>
2024-03-16 18:06:33 +0800

    feat: add stepper

--- frontend/src/app/(authenticated)/service-request-dashboard/_components/stepper.tsx
+++ frontend/src/app/(authenticated)/service-request-dashboard/_components/stepper.tsx
@@ -15,19 +15,31 @@ interface StepProps {
 
 function Step({ name, status }: StepProps) {
   return (
-    <div className="w-full flex flex-col justify-center items-center">
+    <li
+      className={`flex flex-col flex-1 items-center space-y-2 
+    [&:not(:last-child):after]:content-[''] 
+    [&:not(:last-child):after]:relative 
+    [&:not(:last-child):after]:top-[1.5rem] 
+    [&:not(:last-child):after]:left-[50%]
+    [&:not(:last-child):after]:w-[73%]
+    [&:not(:last-child):after]:h-[2px] 
+    ${status === ServiceRequestStatus.COMPLETED ? "[&:not(:last-child):after]:bg-green-300" : "[&:not(:last-child):after]:bg-gray-300"}
+    [&:not(:last-child):after]:-order-1`}
+    >
       <div
         className={cn(
-          `w-10 h-10 flex justify-center items-center ${statusBadgeVariant({
-            status,
-          })}`,
+          `w-[48px] h-[48px] flex justify-center items-center ${statusBadgeVariant(
+            {
+              status,
+            }
+          )}`,
           "rounded-full"
         )}
       >
         <StatusIcon status={status} />
       </div>
-      <p>{name}</p>
-    </div>
+      <p className="text-sm">{name}</p>
+    </li>
   )
 }
 
@@ -37,7 +49,7 @@ interface StepperProps {
 
 export default function Stepper({ steps }: StepperProps) {
   return (
-    <ol className="flex flex-wrap">
+    <ol className="flex flex-wrap py-2">
       {steps?.map((step, index) => (
         <Step key={index} name={step.name} status={step.status} />
       ))}
