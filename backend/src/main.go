e5ca4fcf58b826ee7098d53604b654d2d1695dc5
Joshua <tan.yin.feng@hotmail.com>
2024-03-23 20:09:11 +0800

    refactor: startup sequence

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/joshtyf/flowforge/src/database/client"
 	"github.com/joshtyf/flowforge/src/execute"
 	"github.com/joshtyf/flowforge/src/server"
 )
 
-func main() {
-	srm := execute.NewStepExecutionManager(
+func run() {
+	psqlClient, err := client.GetPsqlClient()
+	if err != nil {
+		panic(err)
+	}
+	defer psqlClient.Close()
+	mongoClient, err := client.GetMongoClient()
+	if err != nil {
+		panic(err)
+	}
+	defer mongoClient.Disconnect(context.Background())
+
+	srm, err := execute.NewStepExecutionManager(
+		mongoClient,
+		psqlClient,
 		execute.WithStepExecutor(execute.NewApiStepExecutor()),
 		execute.WithStepExecutor(execute.NewWaitForApprovalStepExecutor()),
 	)
+	if err != nil {
+		panic(err)
+	}
 	srm.Start()
-
 	http.ListenAndServe(":8080", server.New())
+}
+
+func main() {
+	run()
 }
