6b2a5d5d62bbd56b8f97bb4016a5e3cef67153cc
Joshua <tan.yin.feng@hotmail.com>
2024-03-24 16:56:42 +0800

    refactor: graceful shutdown

--- backend/src/main.go
+++ backend/src/main.go
@@ -2,27 +2,59 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/joshtyf/flowforge/src/database/client"
 	"github.com/joshtyf/flowforge/src/execute"
 	"github.com/joshtyf/flowforge/src/logger"
 	"github.com/joshtyf/flowforge/src/server"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+const (
+	SERVER_SHUTDOWN_GRACE_PERIOD = 10 * time.Second
+)
+
+func gracefulShutdown(svr *http.Server, psqlClient *sql.DB, mongoClient *mongo.Client) func(interface{}) {
+	return func(reason interface{}) {
+		log.Println("Server Shutdown:", reason)
+		ctx, cancel := context.WithTimeout(context.Background(), SERVER_SHUTDOWN_GRACE_PERIOD)
+		defer cancel()
+		if err := svr.Shutdown(ctx); err != nil {
+			log.Println("Error Gracefully Shutting Down API:", err)
+		}
+
+		if err := psqlClient.Close(); err != nil {
+			log.Println("Error Gracefully Shutting Down PSQL Client:", err)
+		}
+
+		ctx, cancel = context.WithTimeout(context.Background(), SERVER_SHUTDOWN_GRACE_PERIOD)
+		defer cancel()
+		if err := mongoClient.Disconnect(ctx); err != nil {
+			log.Println("Error Gracefully Shutting Down Mongo:", err)
+		}
+	}
+}
 
 func main() {
 	psqlClient, err := client.GetPsqlClient()
 	if err != nil {
 		panic(err)
 	}
-	defer psqlClient.Close()
+
 	mongoClient, err := client.GetMongoClient()
 	if err != nil {
 		panic(err)
 	}
-	defer mongoClient.Disconnect(context.Background())
 
+	// Start the Step Execution Manager
 	srm, err := execute.NewStepExecutionManager(
 		mongoClient,
 		psqlClient,
@@ -33,10 +65,32 @@ func main() {
 		panic(err)
 	}
 	srm.Start()
+
+	// Create the server
 	config := &server.ServerConfig{
+		Address:      ":8080",
+		Router:       mux.NewRouter(),
 		PsqlClient:   psqlClient,
 		MongoClient:  mongoClient,
 		ServerLogger: logger.NewLogger(os.Stdout),
 	}
-	http.ListenAndServe(":8080", server.New(config))
+	svr := server.New(config)
+
+	// Start the server
+	srvErrs := make(chan error, 1)
+	go func() {
+		srvErrs <- svr.ListenAndServe()
+	}()
+
+	// Create a channel to listen for shutdown signals
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until a signal is received or the server stops
+	select {
+	case err := <-srvErrs:
+		gracefulShutdown(&svr, psqlClient, mongoClient)(err)
+	case <-done:
+		gracefulShutdown(&svr, psqlClient, mongoClient)("Received Shutdown Signal")
+	}
 }
