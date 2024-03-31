72d5d291b167150d5f4b9651c7e3f978d6aab5c2
Joshua <tan.yin.feng@hotmail.com>
2024-03-31 15:12:00 +0800

    refactor: add logger

--- backend/database/postgres_seed/main.go
+++ backend/database/postgres_seed/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/joshtyf/flowforge/src/database"
 	"github.com/joshtyf/flowforge/src/database/client"
 	"github.com/joshtyf/flowforge/src/database/models"
@@ -8,6 +11,7 @@ import (
 )
 
 func main() {
+	logger := logger.NewServerLog(os.Stdout)
 	c, err := client.GetPsqlClient()
 	if err != nil {
 		panic(err)
@@ -35,10 +39,10 @@ func main() {
 	for i := 0; i < 3; i++ {
 		user, err := database.NewUser(c).Create(&users[i])
 		if err != nil {
-			logger.Error("Error inserting user", map[string]interface{}{"err": err})
+			logger.Error(fmt.Sprintf("unable to insert user: %s", err))
 			panic(err)
 		}
-		logger.Info("Inserted user", map[string]interface{}{"user": user})
+		logger.Info(fmt.Sprintf("inserted user: %v", user))
 	}
 
 	om := models.OrganisationModel{
@@ -48,20 +52,20 @@ func main() {
 
 	org, err := database.NewOrganization(c).Create(&om)
 	if err != nil {
-		logger.Error("Error inserting org", map[string]interface{}{"err": err})
+		logger.Error(fmt.Sprintf("unable to insert org: %s", err))
 		panic(err)
 	}
-	logger.Info("Created organisation and membership", map[string]interface{}{"org": org})
+	logger.Info(fmt.Sprintf("inserted org: %v", org))
 
 	_, err = database.NewMembership(c).Create(&models.MembershipModel{UserId: um2.UserId, OrgId: org.OrgId, Role: models.Admin})
 	if err != nil {
-		logger.Error("Error creating membership", map[string]interface{}{"err": err})
+		logger.Error(fmt.Sprintf("unable to create membership: %s", err))
 		panic(err)
 	}
 
 	_, err = database.NewMembership(c).Create(&models.MembershipModel{UserId: um3.UserId, OrgId: org.OrgId, Role: models.Member})
 	if err != nil {
-		logger.Error("Error creating membership", map[string]interface{}{"err": err})
+		logger.Error(fmt.Sprintf("unable to create membership: %s", err))
 		panic(err)
 	}
 }
