289119b421f87b01b80eb7b7e873d64f6cfaae46
Joshua <tan.yin.feng@hotmail.com>
2024-04-04 00:59:42 +0800

    feat: create org selection page

--- frontend/src/app/(authenticated)/layout.tsx
+++ frontend/src/app/(authenticated)/layout.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { UserContext } from "@/components/contexts/user"
+import Navbar from "@/components/layouts/navbar"
 import { getUserProfile } from "@/lib/auth0"
 import { UserProfile } from "@/types/user-profile"
-import { getCookie } from "cookies-next"
+import { getCookie, hasCookie, setCookie } from "cookies-next"
 import { useRouter } from "next/navigation"
-import { ReactNode, createContext, useEffect, useMemo, useState } from "react"
+import { ReactNode, useEffect, useMemo, useState } from "react"
 
 interface AuthenticatedLayoutProps {
   children: ReactNode
@@ -35,9 +36,46 @@ export default function AuthenticatedLayout({
     }
   }, [isLoggedIn, router, setUserProfile])
 
+  // TODO: Replace with actual organisations
+  const orgs = [
+    {
+      id: 1,
+      name: "Organisation 1",
+    },
+    {
+      id: 2,
+      name: "Organisation 2",
+    },
+  ]
+
   return (
     <UserContext.Provider value={userProfile}>
-      {render && children}
+      {!hasCookie("org_id") ? (
+        <div className="">
+          <Navbar username={userProfile.nickname ?? ""} />
+          <div className="mt-20 flex flex-col justify-center items-center">
+            <p className="mb-8 text-2xl">Your Organisations</p>
+            <div className="border rounded-md w-2/5">
+              <ul className="divide-y divide-slate-200">
+                {orgs.map((org) => (
+                  <li
+                    key={org.id}
+                    className="px-8 py-4 cursor-pointer text-xl hover:text-blue-500"
+                    onClick={(e) => {
+                      setCookie("org_id", org.id)
+                      router.refresh() // Refresh the page to re-render
+                    }}
+                  >
+                    {org.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      ) : (
+        render && children
+      )}
     </UserContext.Provider>
   )
 }
