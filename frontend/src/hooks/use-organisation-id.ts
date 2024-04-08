470d8a33fc6012cc6d841f4628d78578f2075281
Lim Zi Yang <yang7332@gmail.com>
2024-04-08 19:43:19 +0800

    refactor: update org id hook logic

--- frontend/src/hooks/use-organisation-id.ts
+++ frontend/src/hooks/use-organisation-id.ts
@@ -1,13 +1,18 @@
-import { useParams } from "next/navigation"
+import { getCookie } from "cookies-next"
+import { useRouter } from "next/navigation"
 import { useMemo } from "react"
 
 const useOrganisationId = () => {
-  // TODO: Uncomment logic once org id is implemented into URL route
-  // const { organisationId } = useParams()
-  // const organisationIdString = useMemo(
-  //   () => (Array.isArray(organisationId) ? organisationId[0] : organisationId),
-  //   [organisationId]
+  // TODO: Uncomment logic once org id is implemented into cookies
+  // const organisationId = useMemo(
+  //   () => getCookie("organisation_id") as string,
+  //   []
   // )
+  // const router = useRouter();
+  // if (!organisationId) {
+  //   router.push("/organisation")
+  // }
+
   return { organisationId: 1 }
 }
 
