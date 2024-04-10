20ebcfefc13fa17a233a27a35ab43406125dc974
Joshua Tan <tan.yin.feng@hotmail.com>
2024-04-10 10:48:42 +0800

    feat: add render

--- frontend/src/app/(authenticated)/layout.tsx
+++ frontend/src/app/(authenticated)/layout.tsx
@@ -2,7 +2,7 @@
 
 import { getCookie, hasCookie } from "cookies-next"
 import { useRouter } from "next/navigation"
-import { ReactNode, useEffect } from "react"
+import { ReactNode, useEffect, useState } from "react"
 
 interface AuthenticatedLayoutProps {
   children: ReactNode
@@ -12,10 +12,13 @@ export default function AuthenticatedLayout({
   children,
 }: AuthenticatedLayoutProps) {
   const router = useRouter()
+  const [render, setRender] = useState(false)
   useEffect(() => {
     if (!getCookie("loggedIn") || !hasCookie("access_token")) {
       router.push("/login")
+    } else {
+      setRender(true)
     }
   }, [router])
-  return children
+  return render && children
 }
