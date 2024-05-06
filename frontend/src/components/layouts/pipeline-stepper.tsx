e8f36b4a086379cc057c666332f933462e93c1d1
Lim Zi Yang <yang7332@gmail.com>
2024-05-06 21:51:17 +0800

    fix: update pipeline stepper css (#130)

--- frontend/src/components/layouts/pipeline-stepper.tsx
+++ frontend/src/components/layouts/pipeline-stepper.tsx
@@ -17,38 +17,38 @@ interface StepProps {
 function Step({ name, status }: StepProps) {
   return (
     <li
-      className={`flex flex-col space-y-2 py-2 items-center
+      className={`w-[7.5rem] flex flex-col space-y-2 py-2 items-center
     [&:not(:last-child):after]:content-[''] 
     [&:not(:last-child):after]:relative 
     [&:not(:last-child):after]:top-[1rem] 
-    [&:not(:last-child):after]:left-[3.2rem]
-    [&:not(:last-child):after]:w-[3.5rem]
+    [&:not(:last-child):after]:left-[4.7rem]
+    [&:not(:last-child):after]:w-[6rem]
     [&:not(:last-child):after]:h-[2px] 
     ${status === StepStatus.STEP_COMPLETED ? "[&:not(:last-child):after]:bg-green-300" : "[&:not(:last-child):after]:bg-gray-300"}
     [&:not(:last-child):after]:-order-1`}
     >
-      <div
-        className={cn(
-          `w-[2rem] h-[2rem] flex justify-center items-center ${stepStatusBadgeVariant(
-            {
-              status,
-            }
-          )}`,
-          "rounded-full"
-        )}
-      >
-        <TooltipProvider>
-          <Tooltip delayDuration={300}>
-            <TooltipTrigger disabled>
+      <TooltipProvider>
+        <Tooltip delayDuration={300}>
+          <TooltipTrigger disabled>
+            <div
+              className={cn(
+                `w-[2rem] h-[2rem] flex justify-center items-center ${stepStatusBadgeVariant(
+                  {
+                    status,
+                  }
+                )}`,
+                "rounded-full"
+              )}
+            >
               <StepStatusIcon status={status} />
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>{status}</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </div>
-      <p className="text-sm">{name}</p>
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>{status}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <p className="text-sm text-center w-full text-wrap truncate">{name}</p>
     </li>
   )
 }
