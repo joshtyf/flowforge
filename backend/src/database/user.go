c9850fe8587668a1544fe33e5b06188289bf32f1
Joshua Tan <tan.yin.feng@hotmail.com>
2024-05-14 16:16:01 +0800

    feat: GetAllUsers

--- backend/src/database/user.go
+++ backend/src/database/user.go
@@ -28,3 +28,21 @@ func (u *User) GetUserById(user_id string) (*models.UserModel, error) {
 	}
 	return um, nil
 }
+
+func (u *User) GetAllUsers() ([]*models.UserModel, error) {
+	rows, err := u.c.Query(SelectAllUsersStatement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users := make([]*models.UserModel, 0)
+	for rows.Next() {
+		um := &models.UserModel{}
+		if err := rows.Scan(&um.UserId, &um.Name, &um.IdentityProvider, &um.CreatedOn, &um.Deleted); err != nil {
+			return nil, err
+		}
+		users = append(users, um)
+	}
+	return users, nil
+}
