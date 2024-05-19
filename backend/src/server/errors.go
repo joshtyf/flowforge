0a05d2f0d8d209f2de5b7f1201a45349b4ddfd7a
Joshua Tan <tan.yin.feng@hotmail.com>
2024-05-19 21:35:31 +0800

    feat: ErrFailedToApproveServiceRequest

--- backend/src/server/errors.go
+++ backend/src/server/errors.go
@@ -16,6 +16,7 @@ var (
 	ErrServiceRequestNotStarted       = errors.New("service request not started")
 	ErrServiceRequestAlreadyStarted   = errors.New("service request already started")
 	ErrServiceRequestAlreadyCompleted = errors.New("service request already completed")
+	ErrFailedToApproveServiceRequest  = errors.New("failed to approve service request")
 
 	ErrUnableToValidateJWT = errors.New("unable to validate JWT")
 	ErrUnauthorised        = errors.New("user does not have required permissions")
