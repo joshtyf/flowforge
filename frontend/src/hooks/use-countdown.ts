6fec6f07ab00a37055cc997b0257604fa35e937a
Lim Zi Yang <yang7332@gmail.com>
2024-05-27 20:10:49 +0800

    refactor: move use countdown hook to hooks folder

--- frontend/src/hooks/use-countdown.ts
+++ frontend/src/hooks/use-countdown.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState, useEffect } from "react"
 
 export const useCountdown = (duration: number, onComplete: VoidFunction) => {
   const [countdown, setCountdown] = useState(duration)
