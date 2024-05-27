2a673532378252612f309bc45afe2d6950c2f0f2
Lim Zi Yang <yang7332@gmail.com>
2024-05-27 21:37:53 +0800

    feat: add browser focus check

--- frontend/src/hooks/use-countdown.ts
+++ frontend/src/hooks/use-countdown.ts
@@ -4,15 +4,18 @@ export const useCountdown = (duration: number, onComplete: VoidFunction) => {
   const [countdown, setCountdown] = useState(duration)
   useEffect(() => {
     const interval = setInterval(() => {
-      setCountdown((prevCountdown) => {
-        if (prevCountdown - 1 === 0) {
-          clearInterval(interval)
-          onComplete()
-          return 0
-        } else {
-          return prevCountdown - 1
-        }
-      })
+      // Only run timer if browser is focused
+      if (document.hasFocus()) {
+        setCountdown((prevCountdown) => {
+          if (prevCountdown - 1 === 0) {
+            clearInterval(interval)
+            onComplete()
+            return 0
+          } else {
+            return prevCountdown - 1
+          }
+        })
+      }
     }, 1000)
     return () => clearInterval(interval)
   }, [onComplete])
