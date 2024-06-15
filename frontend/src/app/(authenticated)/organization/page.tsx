5f7aa7e2d694bb40d3c9ec9180f8a77625e458f3
Lim Zi Yang <yang7332@gmail.com>
2024-06-15 14:28:58 +0800

    feat: init create org ui

--- frontend/src/app/(authenticated)/organization/page.tsx
+++ frontend/src/app/(authenticated)/organization/page.tsx
@@ -1,20 +1,32 @@
 "use client"
 
 import { Skeleton } from "@/components/ui/skeleton"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import { getAllOrgsForUser } from "@/lib/service"
 import { Organization } from "@/types/organization"
 import { setCookie } from "cookies-next"
+import { Plus } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 export default function OrganizationsPage() {
   const [organizations, setOrganizations] = useState<Organization[]>([])
   const [loading, setLoading] = useState(true)
-  useEffect(() => {
+
+  const fetchOrgs = () => {
     getAllOrgsForUser().then((orgs) => {
       setOrganizations(orgs)
       setLoading(false)
     })
+  }
+
+  useEffect(() => {
+    fetchOrgs()
   }, [])
 
   const router = useRouter()
@@ -42,6 +54,19 @@ export default function OrganizationsPage() {
                 {org.name}
               </li>
             ))}
+            <TooltipProvider>
+              <Tooltip delayDuration={300}>
+                <TooltipTrigger
+                  className="w-full px-8 py-4 cursor-pointer text-xl hover:text-blue-500 flex justify-center items-center"
+                  onClick={() => {}}
+                >
+                  <Plus />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Create New Organization</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </ul>
         </div>
       )}
