98c881646365a26075e8b79bb26438f9b5ff3848
Lim Zi Yang <yang7332@gmail.com>
2024-06-15 16:17:21 +0800

    refactor: success toast to be success variant

--- frontend/src/app/(authenticated)/organization/_hooks/use-create-organization-form.tsx
+++ frontend/src/app/(authenticated)/organization/_hooks/use-create-organization-form.tsx
@@ -38,6 +38,7 @@ export default function useCreateOrganizationForm({
       .then(() => {
         refetchOrgs()
         toast({
+          variant: "success",
           title: "Organization Created Successfully",
           description: (
             <p>
