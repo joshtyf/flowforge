186e492efbdc9e43eee6f029863d5edcc431ec03
Lim Zi Yang <yang7332@gmail.com>
2024-06-16 15:35:19 +0800

    feat: init create new user dialog

--- frontend/src/contexts/current-user-info-context.tsx
+++ frontend/src/contexts/current-user-info-context.tsx
@@ -1,3 +1,12 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
 import { login } from "@/lib/service"
 import { UserInfo } from "@/types/user-profile"
 import { createContext, useContext, useEffect, useState } from "react"
@@ -10,16 +19,40 @@ export function CurrentUserInfoContextProvider({
   children: React.ReactNode
 }) {
   const [userInfo, setUserInfo] = useState<UserInfo>()
+  const [openAlertDialog, setOpenAlertDialog] = useState(false)
 
   useEffect(() => {
     login()
       .then((res) => setUserInfo(res))
-      .catch((err) => console.error(err))
+      .catch((err) => {
+        if (err.response?.status === 404) {
+          // New user detected, prompt to create user in Flowforge
+          setOpenAlertDialog(true)
+          console.log("New user detected")
+        } else {
+          // Unexpected error
+          console.error(err)
+        }
+      })
   }, [])
 
   return (
     <CurrentUserInfoContext.Provider value={userInfo}>
       {children}
+      <AlertDialog open={openAlertDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Welcome to Flowforge!</AlertDialogTitle>
+            <AlertDialogDescription>
+              Since its your first time using Flowforge, we need you to create a
+              username for your account.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction>Create</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </CurrentUserInfoContext.Provider>
   )
 }
