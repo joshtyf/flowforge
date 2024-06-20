2542deaf2ceb401270ebd1ff6f35a082fc3da42a
Lim Zi Yang <yang7332@gmail.com>
2024-06-20 16:46:50 +0800

    feat: add conditional rendering of message on no org id found

--- frontend/src/app/(authenticated)/(main)/settings/organization/page.tsx
+++ frontend/src/app/(authenticated)/(main)/settings/organization/page.tsx
@@ -9,17 +9,28 @@ export default function OrganizationSettingsPage() {
     useOrganization()
   return (
     <div className="space-y-8">
-      <h1 className="text-2xl font-bold">
-        Organization Settings for <i>{organizationName}</i>
-      </h1>
-      <Separator className="w-full my-2" />
-      <div className="w-3/4">
-        <ChangeOrgNameForm
-          organizationId={organizationId}
-          updateOrgNameInCookie={updateOrgNameInCookie}
-        />
-      </div>
-      <Separator className="w-full my-4" />
+      {organizationId ? (
+        <>
+          <h1 className="text-2xl font-bold">
+            Organization Settings for <i>{organizationName}</i>
+          </h1>
+          <Separator className="w-full my-2" />
+          <div className="w-3/4">
+            <ChangeOrgNameForm
+              organizationId={organizationId}
+              updateOrgNameInCookie={updateOrgNameInCookie}
+            />
+          </div>
+          <Separator className="w-full my-4" />
+        </>
+      ) : (
+        <div>
+          <h1 className="text-2xl font-bold">
+            Please select an organization to continue with organization
+            settings.
+          </h1>
+        </div>
+      )}
     </div>
   )
 }
