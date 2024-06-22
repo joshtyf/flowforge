33b8ca8db2876b82572aa7b3290172df6f5d6ce8
Lim Zi Yang <yang7332@gmail.com>
2024-06-22 15:32:14 +0800

    feat: add selected member logic

--- frontend/src/app/(authenticated)/(main)/settings/organization/_components/add-member-dialog.tsx
+++ frontend/src/app/(authenticated)/(main)/settings/organization/_components/add-member-dialog.tsx
@@ -2,14 +2,13 @@ import { Button } from "@/components/ui/button"
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
-import { Search } from "lucide-react"
+import { Search, X } from "lucide-react"
 import useAddMembers from "../_hooks/use-add-members"
 import { UserInfo } from "@/types/user-profile"
 import { useState } from "react"
@@ -39,31 +38,51 @@ export default function AddMemberDialog({
         <DialogHeader>
           <DialogTitle>Add member to the organization</DialogTitle>
         </DialogHeader>
-        <div className="relative">
-          <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input
-            placeholder="Search User by Username"
-            className="pl-8"
-            onChange={(e) => setSearchFilter(e.target.value)}
-          />
-        </div>
-        <div className="max-h-[20rem] overflow-y-auto">
-          <div className="border rounded-md">
-            <ul className="divide-y divide-slate-200">
-              {allUsers?.map((user) => (
-                <li
-                  key={user.user_id}
-                  className="px-4 py-2 cursor-pointer hover:text-blue-500"
-                >
-                  <p>{user.name}</p>
-                </li>
-              ))}
-            </ul>
+
+        {selectedMember ? (
+          <div className="border rounded-md flex items-center">
+            <p className="px-4 py-2">{selectedMember.name}</p>
+            <X
+              size="20"
+              className="ml-auto mr-2 cursor-pointer"
+              onClick={() => setSelectedMember(undefined)}
+            />
           </div>
-        </div>
+        ) : (
+          <>
+            <div className="relative">
+              <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                placeholder="Search User by Username"
+                className="pl-8"
+                onChange={(e) => setSearchFilter(e.target.value)}
+              />
+            </div>
+            <div className="max-h-[20rem] overflow-y-auto">
+              <div className="border rounded-md">
+                <ul className="divide-y divide-slate-200">
+                  {allUsers?.map((user) => (
+                    <li
+                      key={user.user_id}
+                      className="px-4 py-2 cursor-pointer hover:text-blue-500"
+                      onClick={() => {
+                        setSelectedMember(user)
+                        setSearchFilter("")
+                      }}
+                    >
+                      <p>{user.name}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </>
+        )}
         <DialogFooter>
-          <Button type="submit" className="w-full">
-            Add User
+          <Button type="submit" className="w-full" disabled={!selectedMember}>
+            {!selectedMember
+              ? "Select a user"
+              : `Add ${selectedMember.name} to organization`}
           </Button>
         </DialogFooter>
       </DialogContent>
